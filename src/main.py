77dae3821fef84c14a9c6c31d6f95084f528673e
Kevin Pirabaharan <kevinpirabaharan@gmail.com>
2019-04-01 20:39:57 -0400

    Segmenting does bundles of bananers

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 from imageIO import *
 from imthr_lib import*
 from PIL import Image
-# import cv2
+import cv2
 import datetime
 import glob
 import os
@@ -33,27 +33,40 @@ def imageSegment(imagePath, imageName, output):
     startDT = datetime.datetime.now()
 
     #initialize color channels and size variables
-    red, green, blue = imread_colour(imagePath)
+    im = Image.open(imagePath)
+    height, width = im.size
+    img = cv2.imread(imagePath)
+    HSV = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    cv2.imwrite('hsv.jpg',HSV)
+    red, green, blue = imread_colour('hsv.jpg')
     redB = np.zeros((width,height))
     greenB = np.zeros((width,height))
     blueB = np.zeros((width,height))
-    im = Image.open(imagePath)
-    height, width = im.size
     bananaSA = 0
 
-    #find the otsu threshold of the blue channel and then binarized into a black and white image
+    #find the otsu threshold of the red channel and then binarized LAB image into a black and white image
     #the pixels of the banana should be black (0), whilst the rest of the image should be white (255)
+    thr = otsu(red)
+    imgRedOtsu = im2bw(red,thr)
+    imwrite_gray("Red2.jpeg", imgRedOtsu)
+
+    thr = otsu(green)
+    imgGreenOtsu = im2bw(green,thr)
+    imwrite_gray("Green2.jpeg", imgGreenOtsu)
+
     thr = otsu(blue)
     imgBlueOtsu = im2bw(blue,thr)
-    # imwrite_gray("Blue.jpeg", imgBlueOtsu)
+    imwrite_gray("Blue2.jpeg", imgBlueOtsu)
+
+    red, green, blue = imread_colour(imagePath)
 
     #the Black and White image is looked at pixel by pixel
-    #the black pixels of the the image are saved as the color of the original image, whilst the white pixels are turned Black
-    #this allows the banana to keep it's color while the rest of the image is blackened
+    #the white pixels of the the image are saved as the color of the original image, whilst the rest of the image is saved as black pixels
+    #this allows the banana to keep it's color while the rest of the image is blackenes
     for i in range(0, width-1):
         for j in range(0, height-1):
             avg = (float(red[i,j]) + float(blue[i,j]) + float(green[i,j])) / 3
-            if (imgBlueOtsu[i,j] == 0 and avg <= 127):
+            if (imgRedOtsu[i,j] == 255):
                 redB[i,j] = red[i,j]
                 greenB[i,j] = green[i,j]
                 blueB[i,j] = blue[i,j]
@@ -72,12 +85,43 @@ def imageSegment(imagePath, imageName, output):
     #Returns the surface area of the banana for BrownSpot Analysis
     return bananaSA
 
+
+    # for i in range(0, HSV.shape[0]):
+    #     for j in range(0, HSV.shape[1]):
+    #         HSV[i,j][0] = HSV[i,j][0]*1.4
+    #         HSV[i,j][1] = HSV[i,j][0]/2.55
+    #         HSV[i,j][2] = HSV[i,j][0]/2.55
+    #
+    # im4 = cv2.imread(imagePath)
+    #
+    # for i in range(0, HSV.shape[0]):
+    #     for j in range(0, HSV.shape[1]):
+    #         sum = float(HSV[i,j][0]) + float(HSV[i,j][1]) + float(HSV[i,j][2])
+    #         if (sum != 0):
+    #             if (sum > 90):
+    #                 # print("hi")
+    #                 HSV[i,j][0] = 0
+    #                 HSV[i,j][1] = 0
+    #                 HSV[i,j][2] = 0
+    #             else:
+    #                 HSV[i,j][0] = im4[i,j][0]
+    #                 HSV[i,j][1] = im4[i,j][1]
+    #                 HSV[i,j][2] = im4[i,j][2]
+    #
+    # cv2.imwrite("../images/processed/rgbConvert.png", HSV)
+    #
+    # redC, greenC, blueC = imread_colour("../images/processed/rgbConvert.png")
+    # thr = otsu(blueC)
+    # imgBlueCOtsu = im2bw(blueC,thr)
+    # imwrite_gray("Blue3.jpeg", imgBlueCOtsu)
+
+
 # TODO: BrownSpot Analysis
 
 
 #Program loop to run the program
 inputFolder = "../images/raw/"
-data = open("../data/segmentationResults.txt", 'w')
+data2 = open("../data/testing.txt", 'w')
 fileCount = 0
 progExit = False
 while (progExit == False):
@@ -85,11 +129,12 @@ while (progExit == False):
     if (inp == 'f') or (inp == 'F'):
         print("Make sure the image is inside the \'image/raw/\' folder")
         fileName = raw_input("Enter File Name: ")
-        bananaSize = imageSegment(inputFolder + fileName, fileName, data)
+        bananaSize = imageSegment(inputFolder + fileName, fileName, data2)
         print(str(bananaSize))
 
     elif (inp == 't') or (inp == 'T'):
         print("Testing Algorithms...")
+        data = open("../data/segmentationResults.txt", 'w')
         for file in glob.glob(inputFolder + "*.jpg"):
             fileCount += 1
             print("")
